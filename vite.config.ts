40a8771dae7c8a8e3d677bc7fe88bdbecfed0243
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-04-27 15:28:37 +0100

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,5 @@ export default defineConfig({
   build: {
     // generate manifest.json in outDir
     manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: "./src/main.tsx",
-    },
   },
 });
