ddd1d124391fe1452238c3b50d703baddcf5e661
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-04-27 15:00:38 +0100

    Checking connectivity to the backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +7,14 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": "https://questioneer-web-service.onrender.com",
+    },
+  },
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "./src/main.tsx",
     },
   },
 });
