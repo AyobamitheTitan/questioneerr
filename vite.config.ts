99643eb0df0fc2b2f6975b77c68e93a6cb0f2da2
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-06-18 14:27:04 +0100

    guyyy

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/questioneerr/",
-  // server: {
-  //   proxy: {
-  //     "/api": "https://questioneer-web-service.onrender.com",
-  //   },
-  // },
+  base: "/",
+  server: {
+    proxy: {
+      "/api": "https://questioneer-web-service.onrender.com",
+    },
+  },
   build: {
     // generate manifest.json in outDir
     manifest: true,
