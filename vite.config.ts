ba0ec06fd676cb2b9b4f8aea87c36720d657317f
Oluwatimilehin <adeoluoyewo13@gmail.com>
2023-06-25 11:38:06 +0100

    Modified the base in the vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/questioneerr/",
+  base: "https://ayobamithetitan.github.io/questioneerr/",
   server: {
     proxy: {
       "/api": "https://questioneer-web-service.onrender.com",
